da557c33da5244ac07dacac2fb5354847b0dfeb6
LeyderF <lefigueroa24@itp.edu.co>
2026-06-03 21:29:25 -0500

    Fix

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,24 +9,26 @@ import * as path from 'path';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
-        entities: ['dist/**/*.entity.js'],
-        synchronize: config.get('NODE_ENV') !== 'production',
-        logging: config.get('NODE_ENV') === 'production',
+      useFactory: (config: ConfigService) => {
         const dbCaCert = config.get<string>('DB_CA_CERT');
 
-        ssl: dbCaCert
-          ? {
-              ca: fs.readFileSync(path.resolve(__dirname, '../../', dbCaCert)),
-            }
-          : undefined,
-      }),
+        return {
+          type: 'mysql',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_DATABASE'),
+          entities: ['dist/**/*.entity.js'],
+          synchronize: config.get('NODE_ENV') !== 'production',
+          logging: config.get('NODE_ENV') === 'production',
+          ssl: dbCaCert
+            ? {
+                ca: fs.readFileSync(path.resolve(__dirname, '../../', dbCaCert)),
+              }
+            : undefined,
+        };
+      },
     }),
   ],
 })
